e4930fadf19f0b1beb35364a5b2e137260115945
rnktlhr <rinakitluhur10@gmail.com>
2025-10-27 09:27:50 +0700

    menambahkan index.js untuk test database

---
+++ index.js
@@ -0,0 +1,20 @@
+const exppress = require('express');
+const app = exppress();
+const port = 3000;
+const db = require('./models');
+app.use(exppress.json());
+app.use(exppress.urlencoded({ extended: false }));
+app.listen(port, () => {
+  console.log('Server started on port 3000' );
+});
+
+// Test database connection
+db.sequelize.sync()
+  .then(() => {
+    app.listen(port, () => {
+        console.log('Server Started');
+    })
+})
+.catch((err) => {
+ console.log(err);
+});
