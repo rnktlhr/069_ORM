f2cf9b040e598ac62cd3481faa7d73cadf51cb28
rnktlhr <rinakitluhur10@gmail.com>
2025-10-27 09:14:49 +0700

    membuat model komik dan menambahkan id di komik.js

---
+++ models/komik.js
@@ -0,0 +1,9 @@
+module.exports = (sequelize, DataTypes) => {
+    const Komik = sequelize.define('Komik', {
+        id: {
+        type : DataTypes.INTEGER,
+        autoIncrement : true,
+        primaryKey : true
+        },
+       
+}
