3accc74b318cf0864dad7814e3866e45d86afa34
rnktlhr <rinakitluhur10@gmail.com>
2025-10-27 09:53:37 +0700

    menambahkan put di index js

--- index.js
+++ index.js
@@ -38,3 +38,18 @@ app.get('/komik',  async (req, res) => {
     }
 });
 
+app.put('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+    try {
+        const komik = await db.Komik.findyByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: 'Komik not found' });
+        }
+        await komik.update(data);
+        res.send({message: 'Komik updated successfully', komik});
+    } 
+    catch (error) {
+        res.status(500).send(err);
+    } 
+});
